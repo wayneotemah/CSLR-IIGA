667d70bb62f5f08799df6918706097ddf188b688
Wenslous Otema Egesa <74417129+wayneotemah@users.noreply.github.com>
2026-04-24 23:52:55 +0300

    Replace deprecated np.int with np.int64 in collate padding

--- IIGA/dataloader.py
+++ IIGA/dataloader.py
@@ -59,7 +59,7 @@ def collate_fn(data, fixed_padding=None, pad_index=0):
 
         #For sequence of words
         elif(t=='target'):
-            padded_seqs = np.full((len(sequences), max(lengths)), fill_value=pad_index, dtype=np.int)
+            padded_seqs = np.full((len(sequences), max(lengths)), fill_value=pad_index, dtype=np.int64)
 
         for i, seq in enumerate(sequences):
             end = lengths[i]
